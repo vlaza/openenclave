aa4c7c05698dd7c21dd5d3a57089263738bb831d
Dave Thaler <dthaler@microsoft.com>
2019-01-12 11:36:54 -0800

    GUID conversion wizard, work in progress
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ new_platforms/vsextension/ProjectTemplates/oeenclave/user_ta_header_definesHCode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenEnclaveSDK.ProjectTemplates.oeenclave
+{
+    public partial class user_ta_header_definesH
+    {
+        private Guid m_guid1;
+        public user_ta_header_definesH(String guid1) { this.m_guid1 = Guid.Parse(guid1); }
+        public String Guid1String => m_guid1.ToString();
+        public String Guid1Struct => m_guid1.ToString("X");
+    }
+}
---
+++ new_platforms/vsextension/WizardImplementation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TemplateWizard;
+using System.Windows.Forms;
+using EnvDTE;
+using OpenEnclaveSDK.ProjectTemplates.oeenclave;
+using Microsoft.VisualStudio.TextTemplating;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+using System.IO;
+
+namespace OpenEnclaveSDK
+{
+    public class WizardImplementation : IWizard
+    {
+        // This method is called before opening any item that   
+        // has the OpenInEditor attribute.  
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        // This method is only called for item templates, not for project templates.  
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        // This method is called after the project is created.  
+        public void RunFinished()
+        {
+        }
+
+        private static void WriteFile(string outputFolder, string fileName, string contents)
+        {
+            using (var file = new StreamWriter(Path.Combine(outputFolder, fileName)))
+            {
+                file.Write(contents);
+            }
+        }
+
+        public void RunStarted(
+            object automationObject,
+            Dictionary<string, string> replacementsDictionary,
+            WizardRunKind runKind,
+            object[] customParams)
+        {
+            try
+            {
+                string destinationDirectory;
+                replacementsDictionary.TryGetValue("$destinationdirectory$", out destinationDirectory);
+                string opteeDirectory = Path.Combine(destinationDirectory, "optee");
+
+                string guid1;
+                replacementsDictionary.TryGetValue("$guid1$", out guid1);
+
+                var pt1 = new user_ta_header_definesH(guid1);
+                string outputText1 = pt1.TransformText();
+                WriteFile(opteeDirectory, "user_ta_header_defines.h", outputText1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        // This method is only called for item templates, not for project templates.  
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
